aaccf1718bef0a4cb2d039378d5d88b1cbf96148
Jonathan Astwood <jastwood21@gmail.com>
2022-10-29 22:34:41 -0500

    Implemented A Sprinkle of Javascript specifications

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+
+window.onload = function(){
+  submit = document.getElementsByTagName("button")[0]
+  message = document.getElementsByClassName("message")[0]
+  submit.setAttribute('type', 'button') /*Prevents page reload on submit button click*/
+
+
+  submit.addEventListener("click", function(){
+    email = document.getElementsByTagName("input")[0].value
+    if ((email.includes("@")) && (email.includes("."))){message.innerHTML = "Thank you! Your email " +
+      email.toString() + " has been added to our mailing list!"}
+    else{message.innerHTML = "Error: Please enter valid email address."}
+  })
+}
